4ed364ef6868bf0c91b7316942dab8fd8ec264b9
Ahn <licors214@gmail.com>
2018-09-06 15:34:45 +0900

    서블릿으로 만들어진 pageController 를 일반 클래스로 변환(서블릿에 종속되지 않아 재사용성이 높아지기 때문)

--- Web06/src/spms/servlets/DispatcherServlet.java
+++ Web06/src/spms/servlets/DispatcherServlet.java
@@ -1,14 +1,19 @@
 package spms.servlets;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import smps.controls.Controller;
+import smps.controls.MemberAddController;
+import smps.controls.MemberListController;
 import spms.vo.Member;
 
 @WebServlet("*.do")
@@ -24,18 +29,24 @@ public class DispatcherServlet extends HttpServlet {
 		response.setContentType("text/html; charset=UTF-8");
 		String servletPath = request.getServletPath();
 		try {
+			ServletContext sc = this.getServletContext();
+			HashMap<String, Object> model = new HashMap<>();
+			model.put("memberDao", sc.getAttribute("memberDao"));
+			
 			String pageControllerPath = null;
+			Controller pageController = null;
+			
 			if("/member/list.do".equals(servletPath)) {
-				pageControllerPath = "/member/list";
+				pageController = new MemberListController();
 			} 
 			else if("/member/add.do".equals(servletPath)) {
-				pageControllerPath = "/member/add";
+				pageController = new MemberAddController();
+				
 				if(request.getParameter("email") != null) {
-					request.setAttribute("member", new Member()
+					model.put("member", new Member()
 							.setEmail(request.getParameter("email"))
 							.setPassword(request.getParameter("password"))
-							.setName(request.getParameter("name"))
-							);	
+							.setName(request.getParameter("name")));
 				}
 			}
 			else if("/member/update.do".equals(servletPath)) {
@@ -59,16 +70,21 @@ public class DispatcherServlet extends HttpServlet {
 				pageControllerPath = "/auth/logout";
 			}
 			
-			RequestDispatcher rd = request.getRequestDispatcher(pageControllerPath);
-			rd.include(request, response);
+//			RequestDispatcher rd = request.getRequestDispatcher(pageControllerPath);
+//			rd.include(request, response);
 			
-			String viewUrl = (String) request.getAttribute("viewUrl");
+			String viewUrl = pageController.execute(model);
+			
+			for (String key : model.keySet()) {
+				sc.setAttribute(key, model.get(key));
+			}
+			
 			if(viewUrl.startsWith("redirect:")) {
 				response.sendRedirect(viewUrl.substring(9));
 				return;
 			}
 			else {
-				rd = request.getRequestDispatcher(viewUrl);
+				RequestDispatcher rd = request.getRequestDispatcher(viewUrl);
 				rd.include(request, response);
 			}
 		} catch(Exception e) {
